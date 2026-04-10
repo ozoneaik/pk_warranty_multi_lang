a21d526c0b5d3186f7dde492238f7e2f3785c50a
phuwadech <aofphuwadech@gmail.com>
2026-04-10 08:31:35 +0700

    Fix AssignRichMenu

--- app/Console/Commands/AssignRichMenuToOldUsers.php
+++ app/Console/Commands/AssignRichMenuToOldUsers.php
@@ -16,17 +16,21 @@ class AssignRichMenuToOldUsers extends Command
         $richMenuId = 'richmenu-b0bcd26a8e3430bf7375935b5ea98a3f';
         $accessToken = env('LINE_CHANNEL_ACCESS_TOKEN');
 
+        if (empty($accessToken)) {
+            $this->error("LINE_CHANNEL_ACCESS_TOKEN is missing in .env");
+            return;
+        }
+
         // ดึง User เก่าทั้งหมดมาทีละ 100 คนเพื่อไม่ให้เมมโมรี่เต็ม
         TblCustomerProd::whereNotNull('cust_uid')->chunk(100, function ($customers) use ($richMenuId, $accessToken) {
 
-            // LINE มี API แบบ Bulk ที่สามารถผูกทีละหลายๆ คนได้ (สูงสุด 500 คนต่อครั้ง)
-            // POST https://api.line.me/v2/bot/richmenu/{richMenuId}/bulk/link
-
             $userIds = $customers->pluck('cust_uid')->toArray();
 
+            // [แก้ไขตรงนี้] เปลี่ยน URL และย้าย richMenuId มาไว้ในข้อมูลที่ส่งไป (Payload)
             $response = Http::withToken($accessToken)
-                ->post("https://api.line.me/v2/bot/richmenu/{$richMenuId}/bulk/link", [
-                    'userIds' => $userIds
+                ->post("https://api.line.me/v2/bot/richmenu/bulk/link", [
+                    'richMenuId' => $richMenuId,
+                    'userIds'    => $userIds
                 ]);
 
             if ($response->successful()) {
